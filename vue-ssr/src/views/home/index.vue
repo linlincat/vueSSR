742775dd34313b941116e812684309dac23f9eec
cat <none>
2023-04-25 22:35:25 +0800

    首页布局

--- vue-ssr/src/views/home/index.vue
+++ vue-ssr/src/views/home/index.vue
@@ -1,97 +1,70 @@
 <script setup lang="ts">
-import { ElMessage, ElMessageBox, UploadProps, UploadUserFile } from 'element-plus';
-import {useRoute} from 'vue-router'
-import  got  from "./got.vue"
-import { apiHomeList } from '@/api/index'
-import { ref, watch } from 'vue';
-const route = useRoute()
-const activeName = ref("login")
+import axios from 'axios';
+import { onMounted } from 'vue';
 
-const fileList = ref<UploadUserFile[]>([
-  {
-    name: 'element-plus-logo.svg',
-    url: 'https://element-plus.org/images/element-plus-logo.svg',
-  },
-  {
-    name: 'element-plus-logo2.svg',
-    url: 'https://element-plus.org/images/element-plus-logo.svg',
-  },
-])
-
-const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
-  console.log(file, uploadFiles)
-}
-
-const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
-  console.log(uploadFile)
-}
-
-const handleExceed: UploadProps['onExceed'] = (files, uploadFiles) => {
-  ElMessage.warning(
-    `The limit is 3, you selected ${files.length} files this time, add up to ${
-      files.length + uploadFiles.length
-    } totally`
-  )
-}
-
-const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
-  return ElMessageBox.confirm(
-    `Cancel the transfer of ${uploadFile.name} ?`
-  ).then(
-    () => true,
-    () => false
-  )
-}
-const handleChange: UploadProps['onChange'] = (uploadFile, uploadFiles) => {
-  console.log(uploadFile,"upload")
-  console.log(uploadFiles,"uploadFilesuploadFiles")
-  // debugger
-  fileList.value = fileList.value.slice(-3)
-}
-const handleClick = (e: any) => {
-  console.log(e.props.name)
-}
-watch(
-  route,
-  (count, prevCount) => {
-    console.log(count.query)
-    // const ni  = count.query.name;
-    const ni  = "login";
-    activeName.value = ni as string
-  }
-)
-
+onMounted(() => {
+  axios.get('../../../public/json').then(res => {
+  console.log(res.data, '====')
+  })
+})
 </script>
 <template>
-  <el-upload
-    v-model:file-list="fileList"
-    :on-change="handleChange"
-    class="upload-demo"
-    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-    multiple
-    :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :before-remove="beforeRemove"
-    :limit="3"
-    :on-exceed="handleExceed"
-  >
-    <el-button type="primary">Click to upload</el-button>
-    <template #tip>
-      <div class="el-upload__tip">
-        jpg/png files with a size less than 500KB.
-      </div>
-    </template>
-  </el-upload>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="one" name="login">
-      第一个位置
-    </el-tab-pane>
-    <el-tab-pane label="two" name="register">
-      第二个位置
-      <got></got>
-      <!-- <button @click="goto">edit</button> -->
-    </el-tab-pane>
-  </el-tabs>
+  <div class="container">
+    <div class="banner">我是一个广告位</div>
+    <div style="width: 1200px; margin: 0 auto;">
+      <h2 class="title">我是一个标题</h2>
+      <ul class="list">
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+        <li>
+          <div>背景颜色</div>
+          <span>描述</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
